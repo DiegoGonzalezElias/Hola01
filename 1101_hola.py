c6d8efeb4dab4d282d60ee0c19363709bf4f766c
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2018-09-28 19:49:30 +0200

    Hola mundo

---
+++ 1101_hola.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# esto es un comentario
+# no se interpreta
+# 'mete un nombre: ' es un prompt  
+nombre = raw_input('mete un nombre: ')
+print "Hóla", nombre
+raw_input()
